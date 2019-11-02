10049c961a8240439333cda9e1c4ef6623a4c160
AnandKumar1482 <anand.dandi9@gmail.com>
2019-11-02 12:35:51 +0530

    Replace file is added

---
+++ BridgeLabz/JavaPrograms/Replace.java
@@ -0,0 +1,6 @@
+    class Replace{  
+    public static void main(String args[]){  
+    String s1="Bridge";  
+    String s=s1.replace("Bridge","Labz");//replaces all occurrences of "is" to "was"  
+    System.out.println(s);  
+    }}  
